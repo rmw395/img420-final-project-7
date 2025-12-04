4adeb734271302fa556e2e882d865020df9c44cd
xyloquack <jastibelz@gmail.com>
2025-12-04 00:25:56 -0700

    Combat - Made spells disappear on contact with wall

--- waking-of-the-witch/scripts/Spell.cs
+++ waking-of-the-witch/scripts/Spell.cs
@@ -17,10 +17,19 @@ public partial class Spell : RigidBody2D
 		GD.Print("DamageAmount = ", DamageAmount);
 		GetNode<HitBox>("HitBox").DamageAmount = DamageAmount;
 		GD.Print("DamageAmount for HitBox = ", GetNode<HitBox>("HitBox").DamageAmount);
+		ContactMonitor = true;
 	}
 	
 	private void OnHit(Node2D enemy) {
 		_interactionManager.Call("add_status_effect", enemy, AppliedStatusEffect);
 		QueueFree();
+	}
+	
+	private void OnCollision(Node body) {
+		StaticBody2D wall = body as StaticBody2D;
+		if (wall != null) {
+			GD.Print("Hit Wall!");
+			QueueFree();
+		}
 	}
 }
